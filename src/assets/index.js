7fb3fd3db78c0eed8c65a52d8db60b05e52fe3ca
senyameow <arseniyxd@gmail.com>
2023-09-24 17:09:40 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import bg_1 from './bg_1'
+import bg_2 from './bg_2'
+import bg_3 from './bg_3'
+import bg_4 from './bg_4'
+import bg_5 from './bg_5'
+
+export {
+    bg_1,
+    bg_2,
+    bg_3,
+    bg_4,
+    bg_5,
+}
---
+++ src/components/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { bg_1, bg_2, bg_3, bg_4, bg_5 } from '../assets'
+
+const Head = () => {
+    return (
+        <div className='h-screen w-full bg-cover bg-center secion__1'>
+
+        </div>
+    )
+}
+
+export default Head
